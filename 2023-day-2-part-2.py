5d5034a2d05a84cee7023f225d210cb3aeb3b686
Advait T Ajith <90633634+Sha-Dow-Mon-Arch@users.noreply.github.com>
2023-12-02 12:50:23 +0530

    Add files via upload

---
+++ 2023-day-2-part-2.py
@@ -0,0 +1,22 @@
+sam = 0
+for gn in range(1, 101):
+    d = {
+        'red': 0,
+        'blue': 0,
+        'green': 0
+        }
+    w = input().split(':')
+    w = w[1]
+    w = w.split(';')
+    for i in range(len(w)):
+        w[i] = w[i].split(',')
+        for j in range(len(w[i])):
+            w[i][j] = w[i][j].split()
+    flag = 0
+    for turn in w:
+        for col in turn:
+            d[col[1]] = max(d[col[1]], int(col[0]))
+    #print(d)
+    ans = d['red'] * d['blue'] * d['green']
+    sam += ans
+print(sam)
